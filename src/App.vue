e4b80e39b3080fc21879beeaf1885f3343895c32
Eduardo <eleonc@inlearning.edu.pe>
2021-09-27 13:19:57 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+      <Estilito></Estilito>
+    <HelloWorld />
+  
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import estilito from './store/estilito.css'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    estilito
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="hello">
+    <header class="hola">
+      <nav class="oli">
+
+        <div class="mover">
+          <div class="letras">
+          <p class="E">E</p>
+           </div>
+           <div class="letras">
+          <p class="V">V</p>
+          </div>
+          <div class="letras">
+          <p class="A">A</p>
+          </div>
+        </div>
+        
+       
+      </nav>
+    </header>
+    <section>
+      <div class="conteiner">
+        <div class="conen">
+          <ul class="decora">
+          <li><a href="">inicio</a></li>
+           <li><a href="">home</a></li>
+            <li><a href="">insitucion</a></li>
+             <li><a href="">postgrado</a></li>
+              <li><a href="">avanza</a></li>
+        </ul>
+        </div>
+
+        <div class="conteiner2">
+            <div class="titu1">
+              <div> <h1>Reglamentos</h1></div>
+              <div class="ani"><a href="">añadir nueva seccion</a></div>
+               
+            </div>
+            <div class="titu2">
+              <h3 id="cambia">Reglamentos</h3>
+            </div>
+            <div class="titu3">
+              <h3 id="cambia2">Guías y politicas</h3>
+            </div>
+            
+        
+            
+          <div class="caja1">
+            <h3>Reglamento docente</h3>
+            <p>Conoce tus derechos y obligaciones asi como los requisitos y <br>procedimientos que deberas tomar en cuenta para dictar tus clases <br>con exito</p>
+            <div class="des">
+
+              
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+          </div>
+           <div class="caja2">
+             <h3>Reglamento interno de trabajo</h3>
+            <p>Conoce los lineamientos y condiciones que deberás cumplir y respetar 
+              <br>al ser parte de nuestra organizacion.Junto a ti seguiremos </p><p id="unico">#tranformandovidas</p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+          </div>
+           <div class="caja3">
+              <h3>Código de etica</h3>
+            <p>Conoce los principios, valores y comportamientos que nos representan e integran nuestra cultura.¡Somos el ejemplo! </p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+          </div>
+           <div class="caja4">
+             <h3>Reglamento de Prevención y Sanción del Hostigamiento Sexual</h3>
+            <p>Revisa las normas, así como el protocolo para prevenir, investigar y <br>sancionar el Hostigamiento sexual en la organizacion.<br>
+
+              Nuestra postura es tolerancia cero hacia el hostigamiento sexual laboral.
+            </p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+            
+          </div>
+           <div class="caja5">
+                 <h3>Guía de evaluación docente 360°</h3>
+            <p>Conoce las dimensiones y criterios con los que se evaluará tu <br>desempeño semestralmente. Aquí encontrarás la definición de cada <br> aspecto a evaluar,así como su forma de cálculo y los instrumentos <br>utilizados.
+            </p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+             </div>
+               <div class="caja6">
+            <h3>Política de prevencion de la violencia y discriminacion hacia <br> la mujer</h3>
+            <p>Conoce nuestro compromiso en la lucha de una vida sin violencia <br> contra las mujeres <b>#sumamosesfuerzos</b>
+            </p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+          </div>
+           <div class="caja7">
+            <h3>Política de diversidad</h3>
+            <p>Conoce nuestro compromiso en impulsar la diversidad, equidad e <br> inclusión. Valoramos y abrazamos las diferencias.<br><b id="ok">#Másdiversidad #másinclusión #másequidad</b></p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+          </div>
+           <div class="caja8">
+            <h3>Política de Seguridad y Salud en el trabajo</h3>
+            <p>Conoce tus funciones, responsabilidades y las de la organización, con <br>respecto a la Seguridad y Salud en el Trabajo.<br>
+            ¡Promovemos una cultura de prevencíon y mejora continua!
+            </p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+          </div>
+           <div class="caja9">
+            <h3>Política de Sostenibilidad</h3>
+            <p>Conoce nuestro compromiso con el equilibrio entre crecimiento <br>económico, cuidado del medio ambiente y bienestar social,
+            <b id="ok1">#somosresponsables</b>
+            </p>
+            <div class="des">
+              <a href="">Previsualizar</a>
+              <a href="">Descargar</a>
+            </div>
+         
+             </div>
+           </div>
+
+           
+        
+         
+      
+        
+       </div>
+
+           
+    </section>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
